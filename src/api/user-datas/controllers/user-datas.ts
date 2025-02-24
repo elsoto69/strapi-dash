bed14d35fe06e4c470a688d5783009f4b68bfefd
imsotodev <sotopaguayj@gmail.com>
2025-02-23 22:20:54 -0500

    feat: :sparkles: image support add

---
+++ src/api/user-datas/controllers/user-datas.ts
@@ -0,0 +1,7 @@
+/**
+ * user-datas controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::user-datas.user-datas');
---
+++ src/api/user-datas/routes/user-datas.ts
@@ -0,0 +1,7 @@
+/**
+ * user-datas router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::user-datas.user-datas');
